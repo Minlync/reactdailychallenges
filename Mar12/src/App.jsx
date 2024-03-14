9b0ffd4a507e1b932ddfefd9856496e0e54d7619
Minlync <93282772+Minlync@users.noreply.github.com>
2024-03-14 10:34:48 -0400

    Update App.jsx
    clean up

--- Mar12/src/App.jsx
+++ Mar12/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Selector from "./components/Selector";
 
 import './App.css'
@@ -16,7 +14,7 @@ function App() {
       <h1>Selector</h1>
       <div className="card">
         <Selector />
-        <h1> Vite + React + MUI </h1>
+        <h1> Vite + React + MUI + Docker</h1>
         <p> imported components from MUI</p>
       </div>
       <p className="read-the-docs">
